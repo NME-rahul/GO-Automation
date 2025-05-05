ceae85276ad242061270718599758b4f5b9413fc
Rahul verma <100432854+NME-rahul@users.noreply.github.com>
2025-05-05 23:39:05 +0530

    Add files via upload

---
+++ sources.py
@@ -0,0 +1,12 @@
+
+source = [
+"https://gateoverflow.in/1562/gate-cse-2013-question-58",
+"https://gateoverflow.in/411781/gate-mechanical-2022-set-1-ga-question-2", 
+"https://gateoverflow.in/411704/gate-ch-2022-ga-question-10"
+]
+
+
+target_source = [
+"https://gateoverflow.in/468353/go-classes-test-series-2025-nielit-mock-test-1-question-15", "https://gateoverflow.in/468352/go-classes-test-series-2025-nielit-mock-test-1-question-16",
+"https://gateoverflow.in/468351/go-classes-test-series-2025-nielit-mock-test-1-question-17"
+]
