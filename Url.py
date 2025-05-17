2bfe908de3dac5416e5f1d0ef57e5774d6e4d68b
Rahul verma <rv42088@gmail.com>
2025-05-17 14:14:14 +0530

    Tested Copy Q&A, Remaining Paste Q&A

--- Url.py
+++ Url.py
@@ -1,49 +1,10 @@
 # Source from where to copy the url
 source = [
-"https://gateoverflow.in/446455/go-classes-cs-test-series-2025-mock-gate-test-9-question-26",
-"https://gateoverflow.in/446445/go-classes-cs-test-series-2025-mock-gate-test-9-question-32",
-"https://gateoverflow.in/448379/go-classes-cs-2025-all-india-mock-test-2-question-18",
-"https://gateoverflow.in/394574/go-classes-cs-test-series-2025-mock-gate-test-1-question-24",
-"https://gateoverflow.in/456800/go-classes-cs-2025-all-india-mock-test-5-question-61",
-"https://gateoverflow.in/458718/go-classes-da-2025-all-india-mock-test-6-question-59",
-"https://gateoverflow.in/450866/go-classes-cs-2025-all-india-mock-test-3-question-27",
-"https://gateoverflow.in/419202/go-classes-cs-test-series-2025-mock-gate-test-6-question-59",
-"https://gateoverflow.in/452987/go-classes-da-2025-all-india-mock-test-2-question-31",
-"https://gateoverflow.in/1311/gate-cse-2009-question-19",
-"https://gateoverflow.in/1335/gate-cse-2009-question-49",
-"https://gateoverflow.in/1336/gate-cse-2009-question-50",
-"https://gateoverflow.in/388677/go-classes-cs-test-series-2025-mock-gate-14-question-32",
-"https://gateoverflow.in/448303/go-classes-cs-2025-all-india-mock-test-2-question-60",
-"https://gateoverflow.in/417447/go-classes-cs-test-series-2025-mock-gate-test-5-question-41",
-"https://gateoverflow.in/450880/go-classes-cs-2025-all-india-mock-test-3-question-18",
-"https://gateoverflow.in/422294/go-classes-test-series-2024-mock-gate-test-14-question-11",
-"https://gateoverflow.in/454710/go-classes-cs-2025-all-india-mock-test-4-question-33",
-"https://gateoverflow.in/398155/go-classes-cs-test-series-2025-mock-gate-test-4-question-11",
-"https://gateoverflow.in/396732/go-classes-cs-test-series-2025-mock-gate-test-3-question-19",
+"https://gateoverflow.in/44158/isro-2013-48",
 ]
+
 
 # target from where to paste the conent
 target = [
-
-"https://gateoverflow.in/468267/go-classes-test-series-2025-nielit-mock-test-1-question-101",
-"https://gateoverflow.in/468266/go-classes-test-series-2025-nielit-mock-test-1-question-102",
-"https://gateoverflow.in/468265/go-classes-test-series-2025-nielit-mock-test-1-question-103",
-"https://gateoverflow.in/468264/go-classes-test-series-2025-nielit-mock-test-1-question-104",
-"https://gateoverflow.in/468263/go-classes-test-series-2025-nielit-mock-test-1-question-105",
-"https://gateoverflow.in/468262/go-classes-test-series-2025-nielit-mock-test-1-question-106",
-"https://gateoverflow.in/468261/go-classes-test-series-2025-nielit-mock-test-1-question-107",
-"https://gateoverflow.in/468260/go-classes-test-series-2025-nielit-mock-test-1-question-108",
-"https://gateoverflow.in/468259/go-classes-test-series-2025-nielit-mock-test-1-question-109",
-"https://gateoverflow.in/468258/go-classes-test-series-2025-nielit-mock-test-1-question-110",
-"https://gateoverflow.in/468257/go-classes-test-series-2025-nielit-mock-test-1-question-111",
-"https://gateoverflow.in/468256/go-classes-test-series-2025-nielit-mock-test-1-question-112",
-"https://gateoverflow.in/468255/go-classes-test-series-2025-nielit-mock-test-1-question-113",
-"https://gateoverflow.in/468254/go-classes-test-series-2025-nielit-mock-test-1-question-114",
-"https://gateoverflow.in/468253/go-classes-test-series-2025-nielit-mock-test-1-question-115",
-"https://gateoverflow.in/468252/go-classes-test-series-2025-nielit-mock-test-1-question-116",
-"https://gateoverflow.in/468251/go-classes-test-series-2025-nielit-mock-test-1-question-117",
-"https://gateoverflow.in/468250/go-classes-test-series-2025-nielit-mock-test-1-question-118",
-"https://gateoverflow.in/468249/go-classes-test-series-2025-nielit-mock-test-1-question-119",
-"https://gateoverflow.in/468248/go-classes-test-series-2025-nielit-mock-test-1-question-120",
 
 ]
